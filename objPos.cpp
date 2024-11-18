3108fe832da9c520b7d5813e8c1f7d08dd2788ac
lin422-compeng <lin422@mcmaster.ca>
2024-11-18 16:20:51 -0500

    test push

--- objPos.cpp
+++ objPos.cpp
@@ -19,6 +19,20 @@ objPos::objPos(int xPos, int yPos, char sym)
 // Respect the rule of six / minimum four
 // [TODO] Implement the missing special member functions to meet the minimum four rule
 
+objPos::~objPos() //Destructor
+{
+
+}
+
+objPos::objPos(const objPos& obj) //Copy 
+{
+
+}
+
+objPos& objPos::operator= (const objPos& obj)
+{
+    return *this;
+}
 
 
 
