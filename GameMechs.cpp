b29e26654f1690c3b4d27d49dc7fa4fd42bbe813
lin422-compeng <lin422@mcmaster.ca>
2024-11-20 15:16:50 -0500

    basic game mech implement

--- GameMechs.cpp
+++ GameMechs.cpp
@@ -1,13 +1,19 @@
 #include "GameMechs.h"
+#include "MacUILib.h"
 
 GameMechs::GameMechs()
 {
-    
+    boardSizeX = 30;
+    boardSizeY = 15;
 }
 
 GameMechs::GameMechs(int boardX, int boardY)
 {
-    
+    if (boardX < 10) boardX = 30; //Sets board size to default size if values are unreasonably small
+    if (boardY < 5) boardY = 15;
+
+    boardSizeX = boardX;
+    boardSizeY = boardY;
 }
 
 // do you need a destructor?
@@ -18,38 +24,40 @@ GameMechs::~GameMechs()
 
 bool GameMechs::getExitFlagStatus() const
 {
-
+    return exitFlag;
 }
 
 bool GameMechs::getLoseFlagStatus() const
 {
-
+    return loseFlag;
 }
     
 
 char GameMechs::getInput() const
 {
-
+    if(MacUILib_hasChar()) {
+        return MacUILib_hasChar();
+    }
 }
 
 int GameMechs::getScore() const
 {
-
+    return score;
 }
 
-void GameMechs::incrementScore()
+void GameMechs::incrementScore(int val)
 {
-    
+    score += val;
 }
 
 int GameMechs::getBoardSizeX() const
 {
-
+    return boardSizeX;
 }
 
 int GameMechs::getBoardSizeY() const
 {
-
+    return boardSizeY;
 }
 
 
